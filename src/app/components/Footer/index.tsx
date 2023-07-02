66466630ad20ecba1a37367539712edb4788957c
Hubert Krawczyk <hubert0krawczyk@gmail.com>
2023-07-03 00:33:07 +0200

    add github link

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="mt-auto py-6 bg-gray-900 text-center text-base">
       Spotify Lyrics Online 2023
+      <br />
+      <Link href="https://github.com/HubertKrawczyk/spotify-lyrics-online" className="underline text-green-700 hover:text-green-300">GitHub</Link>
     </footer>
   );
 }
